b3645d56c6b276a3721d9f471fea730a3f4fa7f3
Suraj <sksuraj148333@gmail.com>
2021-07-29 00:39:03 +0530

    no change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from './pages/Header'
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./pages/Header";
+import Review from "./pages/Review";
+import {Context} from "./Context/context";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route>
-            <Header />
-          </Route>
-        </Switch>
+        <Context.Provider
+        value={{
+          review: {}
+        }}>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+            <Route path="/review" exact>
+              <Review />
+            </Route>
+          </Switch>
+        </Context.Provider>
       </div>
-    </Router>  
+    </Router>
   );
 }
 
--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Item.css";
+import {useHistory} from 'react-router-dom';
+import {Context} from '../Context/context';
 
 const Item = (props) => {
+  const history = useHistory();
+  const updated = useContext(Context);
   const handleClick = (e) => {
-    let fetchData;
+    // let fetchData;
     let href = {
       href : props.href
     }
     console.log(href);
 
-    try {
-      fetchData = async () => {
-        const data = href;
-        const response = await fetch("http://localhost:5000/review", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-        const field = await response.json();
-        console.log(field.result);
-        // setRes(field.result);
-        if (response.ok) {
-          console.log("done");
-          // setLoad(false);
-        }
-      };
-    } catch (err) {
-      console.log(err);
-    }
+    // try {
+    //   fetchData = async () => {
+    //     const data = href;
+    //     const response = await fetch("http://localhost:5000/review", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(data),
+    //     });
+    //     const field = await response.json();
+    //     console.log(field.result);
+    //     // setRes(field.result);
+    //     if (response.ok) {
+    //       console.log("done");
+    //       // setLoad(false);
+    //       history.push('/review');
+    //     }
+    //   };
+    // } catch (err) {
+    //   console.log(err);
+    // }
 
-    fetchData();
+    // fetchData();
+    updated.review = 
+    history.push('/review')
 
   }
 
